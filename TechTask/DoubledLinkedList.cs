bcd2f20ba1db6596b8b9e8cfe84f3d3733194308
tamerlansb <tamerlan.sagindykov@gmail.com>
2016-10-14 01:13:56 +0300

    version 1.0

---
+++ TechTask/DoubledLinkedList.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTask
+{
+    public class Node<T> {
+        private T data;
+        private Node<T> next;
+        private Node<T> prev;
+        public Node(T Data)
+        {
+            this.data = Data;
+            this.next = null;
+            this.prev = null;
+        }
+        public Node<T> Next
+        {
+            get { return next; }
+            set { next = value;  }
+        }
+        public Node<T> Prev
+        {
+            get { return prev; }
+            set { prev = value; }
+        }
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+    public class DoubledLinkedList<T>
+    {
+        private Node<T> First;
+        private Node<T> Last;
+        private Node<T> Curr;
+        private uint count; 
+        public DoubledLinkedList()
+        {
+            this.count = 0;
+            this.First = this.Last = this.Curr = null;
+        }
+        public void Add(T elem)
+        {
+            Node<T> NewNode = new Node<T>(elem);
+            if (First == null)
+            {
+                First = Last = NewNode;
+            }
+            else
+            {
+                Last.Next = NewNode;
+                NewNode.Prev = Last;
+                count++;
+            }
+        }
+        public void RemoveByPredicate(Predicate<T> pred)
+        {
+            
+        }
+       // Print()
+       // ReversePrint()
+       // AddByIndex()
+       // RemoveByIndex()
+       // RemoveAllByLastName
+       // SaveToFile()
+       // DownloadFromFile()
+    }
+}
---
+++ TechTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTask
+{
+    public class Person
+    {
+        private string lastname;
+        private DateTime dateofbirth;
+        private uint height;
+        public Person(string LastName,uint Height, DateTime DateOfBirth)
+        {
+            this.dateofbirth = DateOfBirth;
+            this.lastname = LastName;
+            this.height = Height;
+        }
+        public string LastName {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return dateofbirth; }
+            set { dateofbirth = value; }
+        }
+        public uint Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
